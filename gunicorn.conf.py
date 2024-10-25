46c7954c679113ca319eca6a979f89b9365a50f1
Madushan-Jaya-Sri <111506676+Madushan-Jaya-Sri@users.noreply.github.com>
2024-10-25 14:19:05 +0530

    cleared all

--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-workers = 3
-bind = "0.0.0.0:8000"
-timeout = 120
-keepalive = 5
-errorlog = "-"
-accesslog = "-"
-loglevel = "info"
--- scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Wait for nginx to be ready
-sleep 5
-
-# Start Gunicorn
-exec gunicorn -c gunicorn.conf.py app.main:app
